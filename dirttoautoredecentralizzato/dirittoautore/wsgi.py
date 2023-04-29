9451640d574db9e93f094a6ee3168db636498c7a
Egrassi1 <112400676+Egrassi1@users.noreply.github.com>
2023-04-29 11:00:39 +0200

    distribuzione

--- dirttoautoredecentralizzato/dirittoautore/wsgi.py
+++ dirttoautoredecentralizzato/dirittoautore/wsgi.py
@@ -10,7 +10,23 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from web3 import Web3
+from dirittoautore import var
+
+from dirittodecent.apps import DirittodecentConfig
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dirittoautore.settings')
+
+
+### startup code
+
+print('Deploy dei contratti')
+w3 = Web3(Web3.HTTPProvider('http://18.193.74.219:8545'))
+w3.eth.default_account = w3.eth.accounts[0] 
+#per il deploy dei contratti si utilizza un portafoglio, in questo caso uso il primo portafoglio di gananche
+DirittodecentConfig.contrattoTesto(w3)
+DirittodecentConfig.ContrattoLicenza(w3)
+var.web3 =w3
+
 
 application = get_wsgi_application()
--- dirttoautoredecentralizzato/static/js/login.js
+++ dirttoautoredecentralizzato/static/js/login.js
@@ -1,4 +1,4 @@
-const url = "http://127.0.0.1:8000"
+const url = "http://13.53.192.147"
 window.addEventListener("load",onload)
 document.getElementById("login").addEventListener("click",login)
 
